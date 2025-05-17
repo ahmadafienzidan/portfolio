7401a87bc1dabd11a0e84ef7ba6e28f1d5c3552e
afienzidan <afienzidan227@gmail.com>
2025-05-17 13:16:06 +0700

    feat: setup react vite heroui tailwind

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Button } from "@heroui/react";
+function App() {
+  return (
+    <>
+      <h1>Hello World!</h1>
+      <Button color="primary">Test Button</Button>
+    </>
+  );
+}
+
+export default App;
